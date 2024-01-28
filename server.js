175bc202174edadf6d28696837515eb068e3ef46
Shruti-bhuyarkar <BHUYARKARSHRUTI4@GMAIL.COM>
2024-01-28 11:20:49 +0530

    updates

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const app = express();
 const port = 3000;
 const path = require('path');
 
+
 const dbConfig = {
     user: 'postgres',
     host: 'localhost',
@@ -23,6 +24,9 @@ dbClient.connect()
 // Middleware to parse JSON data
 app.use(express.json());
 
+// / Serve static files from the 'public' directory
+app.use(express.static('public'));
+
 // Serve the HTML page at the root
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'index.html'));
@@ -31,7 +35,7 @@ app.get('/', (req, res) => {
 // API endpoint to get countries
 app.get('/api/countries', async (req, res) => {
     try {
-        const result = await dbClient.query('SELECT DISTINCT country FROM locations');
+        const result = await dbClient.query('SELECT DISTINCT country FROM  locations');
         const countries = result.rows.map(row => row.country);
         res.json(countries);
     } catch (error) {
@@ -59,7 +63,7 @@ app.get('/api/cities', async (req, res) => {
     const state = req.query.state;
 
     try {
-        const result = await dbClient.query('SELECT city FROM locations WHERE state = $1', [state]);
+        const result = await dbClient.query('SELECT city FROM  locations WHERE state = $1', [state]);
         const cities = result.rows.map(row => row.city);
         res.json(cities);
     } catch (error) {
@@ -68,18 +72,7 @@ app.get('/api/cities', async (req, res) => {
     }
 });
 // Handle form submissions at /submitData
-app.post('/submitData', async (req, res) => {
-    const { country, state, city, zip } = req.body;
 
-    try {
-        // Example: Insert the submitted data into the database
-        await dbClient.query('INSERT INTO your_table_name (country, state, city, zip) VALUES ($1, $2, $3, $4)', [country, state, city, zip]);
-        res.status(200).send('Form data submitted successfully');
-    } catch (error) {
-        console.error('Error submitting form data:', error);
-        res.status(500).send('Internal Server Error');
-    }
-});
 
 app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
